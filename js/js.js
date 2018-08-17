791f8a6bd6af62258e8f0179d4b349cf7fc53c12
Ryan Snyder <ryansnyder.me@gmail.com>
2018-08-16 17:36:59 -0700

    js, tracking, show / hide list

--- js/js.js
+++ js/js.js
@@ -1,22 +1,68 @@
+
 $('#backToTop').click(function(){
   $('html,body').animate( { scrollTop: 0 } ,'slow');
   return false;
 });
 
+$('#hide-list').click(function(){
+  hideTableRows();
+  $('#hide-list').show();
+  $('#show-list').hide();
+  if (isProductionEnvironment()) {
+    mixpanel.track("Hide shopping list");	
+  }
+});
+
+$('#show-list').click(function(){
+  showTableRows();
+  $('#hide-list').show();
+  $('#show-list').hide();
+  mixpanel.track("Show shopping list");	
+});
+
 $('.soft-scroll').click(function(){
-  mixpanel.track("Click list item");
+  if (isProductionEnvironment()) {
+    mixpanel.track("Click list item");	
+  }  
+
+  var href = $(this).attr('href');
+  href = href.replace('#','');
+
+  smoothScrollTo(href);
 });
 
 $('.click-purchase-link').click(function(){
-  mixpanel.track("Click purchase link");
+  if (isProductionEnvironment()) {
+    mixpanel.track("Click purchase link");	
+  }  
 });
 
 // Native smooth scroll to element
-function smoothScrollTo(node) {
-  node.scrollIntoView({behavior: 'smooth'});
+function smoothScrollTo(elementId) {
+  var element = document.getElementById(elementId);
+  element.scrollIntoView({ behavior: "smooth" });	
+}
+
+function isProductionEnvironment() {
+  if (location.hostname == "pepperslist.co") {
+    return true;	
+  }
+  return false;
+}
+
+function hideTableRows() {
+  $(".hide-initially").hide('fast');      		
+}
+
+function showTableRows() {
+  $(".hide-initially").show('fast');	
 }
 
 $(document).ready(function() {
+  if (isProductionEnvironment()) {
+    mixpanel.track("Page load");	
+  }
+
   $('#shopping-list').DataTable( {
       // "order": [[ 1, "asc" ], [2,'asc'], [0,'asc']],
 	  "order": [ 0, "asc" ],
@@ -26,5 +72,8 @@ $(document).ready(function() {
   $("#shopping-list_length").hide();
   $("#shopping-list_info").hide();      
   $("#shopping-list_paginate").hide(); 
-  $("#shopping-list_filter").hide();           	
+  $("#shopping-list_filter").hide();   
+
+  hideTableRows();
 } );
+
